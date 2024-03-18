99aa880a65474d63dd3370206918f7073a05f21c
dev-timm <timm.kuemmeth@gmail.com>
2024-03-18 17:46:20 +0000

    add routing and adjust NavBar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,24 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.svg';
 import Button from 'react-bootstrap/Button'
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return <Navbar className={styles.NavBar} collapseOnSelect expand="md" fixed="top">
         <Container>
-            <Navbar.Brand>
-                <img src={logo} alt="logo" height="32" /></Navbar.Brand>
+            <NavLink to="/">
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="32" />
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav" className="text-left">
                 <Nav className="mr-auto">
-                    <Nav.Link>Home</Nav.Link>
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">Home</NavLink>
                 </Nav>
                 <Nav>
-                    <Nav.Link><Button variant="success">Log In</Button></Nav.Link>
-                    <Nav.Link><Button variant="outline-success">Sign Up</Button></Nav.Link>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login"><Button variant="success">Log In</Button></NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup"><Button variant="outline-success">Sign Up</Button></NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Container>
