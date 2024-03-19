d94a0013b5be7e7a777d5fa216e8564cafe37b00
dev-timm <timm.kuemmeth@gmail.com>
2024-03-19 11:08:36 +0000

    add axios to connect project with drf api

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://drf-api-plantlife-bdbe99797603.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/formdata'
+axios.defaults.withCredentials = true
