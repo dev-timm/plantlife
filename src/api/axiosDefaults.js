5e7f656c77a9ea4b6fad5048ed5152eb589779fe
dev-timm <timm.kuemmeth@gmail.com>
2024-03-21 11:56:14 +0000

    refresh access tokens so user stays logged in

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-plantlife-bdbe99797603.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/formdata'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://drf-api-plantlife-bdbe99797603.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/formdata';
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
