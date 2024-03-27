1e6e6cf2dd63b744eda1b56d22d77863d5bacf33
dev-timm <timm.kuemmeth@gmail.com>
2024-03-27 12:06:24 +0000

    add functionality to search for posts and users on post list

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -6,22 +6,30 @@ import Container from "react-bootstrap/Container";
 
 import Post from "./Post";
 
+import Image from 'react-bootstrap/Image'
+
 import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
+
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.svg";
+import SearchIcon from "../../assets/icon-search.svg"
 import Asset from "../../components/Asset";
+import { Form } from "react-bootstrap";
 
-function PostsPage(message, filter = "") {
+function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
 
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const { data } = await axiosReq.get(`/posts/?${filter}`);
+                const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
                 setPosts(data);
                 setHasLoaded(true);
             } catch (err) {
@@ -30,14 +38,33 @@ function PostsPage(message, filter = "") {
         };
 
         setHasLoaded(false);
-        fetchPosts();
-    }, [filter, pathname]);
+        const timer = setTimeout(() => {
+            fetchPosts();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [filter, query, pathname]);
 
 
     return (
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles mobile</p>
+                <Image className={styles.SearchIcon} src={SearchIcon} />
+                <Form
+                    className={styles.SearchBar}
+                    onSubmit={(event) => event.preventDefault()}
+                >
+                    <Form.Control
+                        value={query}
+                        onChange={(event) => setQuery(event.target.value)}
+                        type="text"
+                        className="mr-sm-2"
+                        placeholder="Search for posts and users"
+                    />
+                </Form>
                 {hasLoaded ? (
                     <>
                         {posts.results.length ? (
