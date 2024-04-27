c4a392d1501d09b063bbfd37bd7f30acd92e5ef8
dev-timm <timm.kuemmeth@gmail.com>
2024-04-27 08:54:15 +0000

    add warning modal before deleting a comment

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import Button from "react-bootstrap/Button";
+import Modal from 'react-bootstrap/Modal'
 import CommentEditForm from "./CommentEditForm";
 import styles from "../../styles/Comment.module.css";
+import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { MoreDropdown } from "../../components/MoreDropdown";
 import { axiosRes } from "../../api/axiosDefaults";
@@ -13,6 +16,8 @@ const Comment = (props) => {
     const { profile_id, profile_image, owner, updated_on, content, id, setPost, setComments } = props;
 
     const [showEditForm, setShowEditForm] = useState(false);
+
+    const [deleteModalShow, setDeleteModalShow] = useState(false);
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
@@ -37,6 +42,33 @@ const Comment = (props) => {
             console.log(err)
         }
     };
+
+    function DeleteModal(props) {
+        return (
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Delete Advertisement
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="mt-2">
+                    <h5>Delete "{content}"</h5>
+                    <p>
+                        Are you sure you want to delete this advertisement? This action can't be undone!
+                    </p>
+                    <Button className={`${btnStyles.Button} ${btnStyles.Primary} float-right ml-2`} onClick={handleDelete} >
+                        Delete
+                    </Button>
+                    <Button className={`${btnStyles.Button} ${btnStyles.Secondary} float-right ml-2`} onClick={() => setDeleteModalShow(false)}>Cancel</Button>
+                </Modal.Body>
+            </Modal>
+        );
+    }
 
     return (
         <>
@@ -64,9 +96,13 @@ const Comment = (props) => {
                 {is_owner && !showEditForm && (
                     <MoreDropdown
                         handleEdit={() => setShowEditForm(true)}
-                        handleDelete={handleDelete}
+                        handleDeleteModal={() => setDeleteModalShow(true)}
                     />
                 )}
+                <DeleteModal
+                    show={deleteModalShow}
+                    onHide={() => setDeleteModalShow(false)}
+                />
             </Media>
         </>
     );
